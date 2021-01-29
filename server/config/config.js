64605cc1dfb826a83567308131bc99061dd130af
Manuel J <manuel.j.coronilla@gmail.com>
2021-01-29 01:12:42 +0100

    Variable entorno heroku

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:fyAj2cMmHfKtp2UZ@cluster0.gzugy.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
